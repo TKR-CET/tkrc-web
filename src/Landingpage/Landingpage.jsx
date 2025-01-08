0108e28d7860d485906b92c9855d5b1baf640052
TKR-CET <vinaybyagari07@gmail.com>
2025-01-08 14:32:00 +0530

    Update Landingpage.jsx

--- src/Landingpage/Landingpage.jsx
+++ src/Landingpage/Landingpage.jsx
@@ -5,6 +5,7 @@ import VideoSection from '../Components/VideoSection/VideoSection.jsx';
 import Footer from '../Components/Footer/Footer.jsx';
 import MobileNav from "../Components/MobileNav/MobileNav.jsx"
 import "./Landingpage.css"
+import Mainfooter from "Components/MainFooter/MainFooter.jsx"
 import DelegatesInfo from "../Components/DelegatesInfo/DelegatesInfo.jsx"
 const Landingpage=()=>{
   return(
@@ -18,10 +19,13 @@ const Landingpage=()=>{
         <div class="mob-nav">
         <MobileNav/>
         </div>
-        
+      <div class="fot">
+        <Mainfooter/>
+        </div>
         <VideoSection/>
-        
+              <div class="del">
           <DelegatesInfo/>
+                </div>
     </div>
     )
 }
