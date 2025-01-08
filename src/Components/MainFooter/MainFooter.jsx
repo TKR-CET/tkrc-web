bbc54575695742c33af85295e5ec2f266d0548d5
TKR-CET <vinaybyagari07@gmail.com>
2025-01-08 16:14:39 +0530

    Update MainFooter.jsx

--- src/Components/MainFooter/MainFooter.jsx
+++ src/Components/MainFooter/MainFooter.jsx
@@ -3,7 +3,7 @@ import './MainFooter.css';
 
 const MainFooter = () => {
     return (
-        <footer className="footer" id="f2" >
+        <footer className="footer">
             <p>Copyright © 2024 TKR College of Engineering & Technology. All Rights Reserved.</p>
             <p>Designer, Developer & Maintenance - Mr. Md. Shakeel (TKRES)</p>
         </footer>
