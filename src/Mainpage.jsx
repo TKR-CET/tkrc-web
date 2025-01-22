1db4b5aa2ab77d92da23a3ba6b0ea05405d15218
TKR-CET <vinaybyagari07@gmail.com>
2025-01-22 11:47:28 +0530

    Update Mainpage.jsx

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -15,7 +15,7 @@ function Mainpage() {
   const location = useLocation();
 
   // List of routes where the other components should be hidden
-  const hideComponentsPaths = ['/timetable', '/index','/attendance','/activity','/home','/mark']; // Add more paths here
+  const hideComponentsPaths = ['/timetable', '/index','/attendance','/activity','/home','/mark','/register']; // Add more paths here
 
   return (
     <div className="maindiv">
