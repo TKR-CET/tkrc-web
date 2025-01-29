a05f4c8432c5b495cb9fe1639ad5a6530a66f55d
TKR-CET <vinaybyagari07@gmail.com>
2025-01-29 11:07:15 +0530

    Update Homepage.jsx

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -91,7 +91,7 @@ const navigate = useNavigate();
         if (facultyResponse.data.success) {
             const faculty = facultyResponse.data.faculty;
            const id= localStorage.setItem("facultyId", faculty._id);
-            alert("id");
+            alert(id);
             alert(`Login successful!\nName: ${faculty.name}\nRole: ${faculty.role}\nDepartment: ${faculty.department}`);
             navigate('/index');
             return; // Exit function if login is successful
