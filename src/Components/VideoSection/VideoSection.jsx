268250a11f2de076e1ff17098f122c114980292b
TKR-CET <vinaybyagari07@gmail.com>
2025-06-19 06:24:14 +0530

    Update VideoSection.jsx

--- src/Components/VideoSection/VideoSection.jsx
+++ src/Components/VideoSection/VideoSection.jsx
@@ -5,14 +5,9 @@ import './VideoSection.css';
 function VideoSection() {
   return (
     <div className="video-section">
-      <video
-        src="./images/tkr.mp4"
-        autoPlay
-        loop
-        muted
-        className="video"
-
-      ></video>
+      <div className="campus-image">
+                        <img src="" />
+                    </div>
     </div>
   );
 }
